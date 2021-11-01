f54f00e2d4d1a1bb5be96f922119f12969da44b0
eason.nb <eason.nb@gmail.com>
2021-11-01 23:09:36 +0800

    start

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,63 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      <img 
+        class="animate__animated animate__flash"
+        src="https://upload.cc/i1/2019/11/20/iBj2Tn.png">
+    </div>
+
+    <div>
+      <img 
+        class="animate__animated animate__bounce" 
+        src="https://upload.cc/i1/2019/11/20/iBj2Tn.png">
+    </div>
+
+    <div>
+      <img 
+        class="animate__animated animate__pulse" 
+        src="https://upload.cc/i1/2019/11/20/qjDM9B.png">
+    </div>
+
+
+    <div>
+      <img 
+        class="animate__animated animate__rubberBand" 
+        src="https://upload.cc/i1/2019/11/20/m96fkK.png">
+    </div>
+
+    <div>
+      <img 
+        class="animate__animated animate__shakeX" 
+        src="https://upload.cc/i1/2019/11/20/qwASWf.png">
+    </div>
+
+    <div>
+      <img
+        class="animate__animated animate__shakeY" 
+        src="https://upload.cc/i1/2019/11/20/kiLTqC.png">
+    </div>
+
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<style lang="sass">
+@import '~animate.css' // 二法澤一(b:1/1)
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+*
+  padding: 0
+  margin: 0
+  box-sizing: border-box
+  
+.home div
+  height: 100vh
+  padding-top: 50px
+  overflow: hidden
+
+.home div:nth-child(even)
+  background-color: #aaa
+
+img
+  display: block
+  margin: 0 auto
+
+</style>
